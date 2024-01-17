2c9f2705d5ef6f6ea0100d2f219a50e1cbd38a46
65160262 <156187577+65160262@users.noreply.github.com>
2024-01-17 19:17:37 +0700

    comment#5
    toggle รายการ Todo

--- lab07/scripts.js
+++ lab07/scripts.js
@@ -23,8 +23,12 @@ document.addEventListener("DOMContentLoaded", function () {
     function deleteTodo(index) {
         todos.splice(index, 1);
         renderTodoList();
-        }    
-
+        }
+    // ตรวจสอบ/ยกเลิกการเสร็จสิ้นรายการ Todo
+    function toggleComplete(index) {
+        todos[index].completed = !todos[index].completed;
+        renderTodoList();
+        }
 
 // แสดงรายการ Todo บนหน้าเว็บ
 function renderTodoList() {
